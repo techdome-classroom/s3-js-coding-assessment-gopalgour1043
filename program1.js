dfc9d4e0154223921a61288563d254c1cc389d3e
Gopal Gour <gopalgour1043@gmail.com>
2024-09-30 15:25:54 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -3,7 +3,26 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    let size = s.length;
+    let st = [];
+
+    if (size === 1) return false;
+
+    for (let i = 0; i < size; i++) {
+        if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
+            st.push(s[i]);
+        } else {
+            if (st.length === 0) return false;
+            if ((s[i] === ')' && st[st.length - 1] === '(') ||
+                (s[i] === '}' && st[st.length - 1] === '{') ||
+                (s[i] === ']' && st[st.length - 1] === '[')) {
+                st.pop();
+            } else {
+                return false;
+            }
+        }
+    }
+    return st.length === 0;
 };
 
 module.exports = { isValid };
