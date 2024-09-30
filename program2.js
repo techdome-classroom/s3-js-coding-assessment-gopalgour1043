d3897f29a4cdaa418b0cf6227feb0317ef040842
Gopal Gour <gopalgour1043@gmail.com>
2024-09-30 15:26:41 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -3,7 +3,27 @@
  * @return {number}
  */
 var romanToInt = function(s) {
+    const m = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
     
+    let ans = 0;
+    
+    for (let i = 0; i < s.length; i++) {
+        if (m[s[i]] < m[s[i + 1]]) {
+            ans -= m[s[i]];
+        } else {
+            ans += m[s[i]];
+        }
+    }
+
+    return ans;
 };
 
 
